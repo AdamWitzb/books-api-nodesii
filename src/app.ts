1927c8cba28092646848c3e8d46bcb3553547de7
Wojciech Sromek <wojciech.sromek@codete.com>
2021-01-16 18:45:40 +0100

    Fourth step: GET /books endpoint (in-memory storage)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+
+type Book = {
+    id: number;
+    authors: string | string[];
+    title: string;
+}
+
+let books: Book[] = [
+    {id: 1, authors: 'Jonathan Haidt', title: 'Coddling of the American Mind'},
+    {id: 2, authors: ['Dan Heath', 'Chip Heath'], title: 'Switch: How to change when change is hard'},
+    {id: 3, authors: 'Kathy Sierra', title: 'Badass: Making users awesome'},
+    {id: 4, authors: 'Daniel Kahneman', title: 'Thinking fast, thinking slow'},
+    {id: 5, authors: 'Caroline Dweck', title: 'Mindset1'},
+    {id: 6, authors: 'Michael Walker', title: 'Why we sleep?'},
+];
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.get('/books', (req: Request, res: Response) => {
+    res.status(200).json(books);
+});
 
 app.listen(3000, () => console.log('app listening'));
