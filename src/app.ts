f5a2e82b50917a24d981df3f1a0e748f3dfe8232
Wojciech Sromek <wojciech.sromek@codete.com>
2021-01-17 20:06:44 +0100

    Ninth step: Implemented tests using mocha and added prettier for keeping consistent style between files

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 
-type Book = {
-    id: number;
-    authors: string | string[];
-    title: string;
-}
+export type Book = {
+  id: number;
+  authors: string | string[];
+  title: string;
+};
 
 let books: Book[] = [
-    {id: 1, authors: 'Jonathan Haidt', title: 'Coddling of the American Mind'},
-    {id: 2, authors: ['Dan Heath', 'Chip Heath'], title: 'Switch: How to change when change is hard'},
-    {id: 3, authors: 'Kathy Sierra', title: 'Badass: Making users awesome'},
-    {id: 4, authors: 'Daniel Kahneman', title: 'Thinking fast, thinking slow'},
-    {id: 5, authors: 'Caroline Dweck', title: 'Mindset1'},
-    {id: 6, authors: 'Michael Walker', title: 'Why we sleep?'},
+  { id: 1, authors: 'Jonathan Haidt', title: 'Coddling of the American Mind' },
+  {
+    id: 2,
+    authors: ['Dan Heath', 'Chip Heath'],
+    title: 'Switch: How to change when change is hard',
+  },
+  { id: 3, authors: 'Kathy Sierra', title: 'Badass: Making users awesome' },
+  { id: 4, authors: 'Daniel Kahneman', title: 'Thinking fast, thinking slow' },
+  { id: 5, authors: 'Caroline Dweck', title: 'Mindset' },
+  { id: 6, authors: 'Michael Walker', title: 'Why we sleep?' },
 ];
 
 const app = express();
@@ -20,38 +24,38 @@ const app = express();
 app.use(express.json());
 
 app.get('/books', (req: Request, res: Response) => {
-    res.status(200).json(books);
+  res.status(200).json(books);
 });
 
 app.get('/books/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
+  const id = Number(req.params.id);
 
-    res.status(200).json(books.filter(book => book.id === id));
+  res.status(200).json(books.filter((book) => book.id === id));
 });
 
 app.post('/books', (req: Request, res: Response) => {
-    const body = req.body;
+  const body = req.body;
 
-    books.push(body);
+  books.push(body);
 
-    res.sendStatus(201);
+  res.sendStatus(201);
 });
 
 app.put('/books/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-    const body = req.body;
+  const id = Number(req.params.id);
+  const body = req.body;
 
-    books = books.map(book => book.id === id ? body : book);
+  books = books.map((book) => (book.id === id ? body : book));
 
-    res.sendStatus(202);
+  res.sendStatus(202);
 });
 
 app.delete('/books/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
+  const id = Number(req.params.id);
 
-    books = books.filter(book => book.id !== id);
+  books = books.filter((book) => book.id !== id);
 
-    res.sendStatus(202);
+  res.sendStatus(202);
 });
 
-app.listen(3000, () => console.log('app listening'));
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import app from './app';
+
+app.listen(3000, () => console.log('API listening on port 3000'));
