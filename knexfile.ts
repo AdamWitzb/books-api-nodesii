e671e0012266cdaf3d07a3d8f6874563f2cd9e64
Wojciech Sromek <wojciech.sromek@codete.com>
2021-01-26 20:37:51 +0100

    Twefth step: Fixing tests step 3

--- knexfile.ts
+++ knexfile.ts
@@ -10,4 +10,16 @@ export default {
       directory: './data/seeds',
     },
   },
+  testing: {
+    client: 'pg',
+    //if in Docker use postgres, if in local use localhost - for both modify /etc/hosts and point postgres to 127.0.0.1
+    connection:
+      'postgres://api:development_pass@postgres:5432/books-api-testing',
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
